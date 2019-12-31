3bdeea61c3924480c2504edf4911d8ead618917c
ReturnSweetPork <happylovetkd@naver.com>
2019-12-31 15:56:59 +0900

    만드는중....12.31 마지막 커밋

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types"
+
+function Food({fav}){
+return <h1>i like {fav}</h1>
+}
+
+const foodILike = []
+
+function renderFood(dish){
+  return <Food name={dish.name} picture={dish.image} />
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(renderFood)}
+
     </div>
   );
 }
 
 export default App;
+
